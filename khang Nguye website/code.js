1f66acdfd9160c44f8867f0a50c9fa9a2e952dfc
Khang Nguyen <62628865+khangaerospace@users.noreply.github.com>
2022-12-07 21:53:17 -0500

    This is my website

---
+++ khang Nguye website/code.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        $('.header');
+    });
+
+
+    var typed = new Typed(".type",{strings: ["am here", "am a Guidance, Navigation and Control Software Developer","do Code"," am an Engineering Student","build Satellite", "do research", "am an Aerospace Engineer"], typeSpeed: 100, backSpeed: 70, loop: true });
+    var typed2 = new Typed(".typenew",{strings: ["Awards","Achivements","Accomplismesnts","Sport Achivements", "Academic Achievement"], typeSpeed: 100, backSpeed: 70, loop: true });
+})
