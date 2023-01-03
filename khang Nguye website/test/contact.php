e8ca29be6ee7bd85d0143cf2284305617c766ffc
Khang Nguyen <62628865+khangaerospace@users.noreply.github.com>
2023-01-03 11:10:00 -0500

    update
    new version

---
+++ khang Nguye website/test/contact.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Khang Nguyen </title>
+        
+        
+        <link rel="stylesheet" href="style.css">
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.12/typed.min.js"></script>
+        
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <link href="contact-form.css" rel="stylesheet">
+        
+
+    </head>
+
+
+    <body>
+        
+    
+        <header>
+            
+            <!-- <span><u>Khang Nguyen</u></span>-->
+            <div class="container">
+            
+                    <img src="logo3.JPG" width="300" height="110" alt="logo" class="logo" >
+    
+            </div>
+            <nav class="nav">
+
+                
+                <ul>
+                    <li><a href="index.html">About me</a></li>
+                    <li><a href="career.html">Career</a></li>
+                    <li><a href="project.html">Projects</a></li>
+                    <li><a href="award2.html">Awards</a></li>
+                    <li><a href="blog.html">Blog</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                    
+                    
+                    
+                </ul>
+
+                <div class="menu-btn">
+                    
+                    <i class="fas fa-bars"></i>
+                </div>
+                
+            </nav>
+        </div>
+    </header>
+        <div>Version 1 BETA</div>
+        
+        
+        <div class="fcf-body">
+
+            <div id="fcf-form">
+            <h3 class="fcf-h3">Contact us</h3>
+        
+            <form id="fcf-form-id" class="fcf-form-class" method="post" action="contact-form-process.php">
+                
+                <div class="fcf-form-group">
+                    <label for="Name" class="fcf-label">Your name</label>
+                    <div class="fcf-input-group">
+                        <input type="text" id="Name" name="Name" class="fcf-form-control" required>
+                    </div>
+                </div>
+        
+                <div class="fcf-form-group">
+                    <label for="Email" class="fcf-label">Your email address</label>
+                    <div class="fcf-input-group">
+                        <input type="email" id="Email" name="Email" class="fcf-form-control" required>
+                    </div>
+                </div>
+        
+                <div class="fcf-form-group">
+                    <label for="Message" class="fcf-label">Your message</label>
+                    <div class="fcf-input-group">
+                        <textarea id="Message" name="Message" class="fcf-form-control" rows="6" maxlength="3000" required></textarea>
+                    </div>
+                </div>
+        
+                <div class="fcf-form-group">
+                    <button type="submit" id="fcf-button" class="fcf-btn fcf-btn-primary fcf-btn-lg fcf-btn-block">Send Message</button>
+                </div>
+                
+                
+        
+            </form>
+            </div>
+        
+        </div>
+            
+            
+            
+            
+            
+            
+        
+        <div class="footer-basic">
+            <footer>
+                <div class="social"><a href="#"><i class="icon ion-social-linkedin"></i></a><a href="#"><i class="icon ion-social-github"></i></a><a href="#"><i class="icon ion-social-youtube"></i></a><a href="#"><i class="icon ion-social-google"></i></a></div>
+                <ul class="list-inline">
+                        <li class="list-inline-item"><a href="index.html">About</a></li>
+                        <li class="list-inline-item"><a href="experience.html">Career</a></li>
+                        <li class="list-inline-item"><a href="project.html">Projects</a></li>
+                        <li class="list-inline-item"><a href="awards.html">Awards</a></li>
+                        <li class="list-inline-item"><a href="blog.html">Blog</a></li>
+                        <li class="list-inline-item"><a href="contact.html">Contact</a></li>
+                
+                <p class="copyright">Made by: Khang Nguyen © 2021</p>
+            </footer>
+        </div>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+    </div>
+
+
+
+    </body>
+
+
+</html>
